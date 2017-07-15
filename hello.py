9c997187fe571bd27700490cf000fbe2c08c0a3b
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-07-15 13:39:50 -0700

    update

--- hello.py
+++ hello.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
 	# return "hello world"
-	return render_template('index.html', name="Kev")
+	return render_template('index.html')
 
-# @app.route('/success')
-# def success():
-# 	return "success"	
+@app.route('/success')
+def success():
+	return render template("success.html")	
+
+
+@app.route('/survey', methods=['POST'])
+def survey():
+	print request.form['name']
+	session['survey_info'] = request.form
+	return redirect('/success')
+
+
 
 
 app.run(debug=True)	
