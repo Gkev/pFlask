99f6fdcffdb488d1d29635b2d5a8349fc65ee73e
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-07-15 14:23:57 -0700

    survey assignment

---
+++ dojo_survey/hello.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'what time is it?'
+
+
+@app.route('/')
+def hello_world():
+	# return "hello world"
+	return render_template('index.html')
+
+
+
+@app.route('/survey', methods=['POST'])
+def survey():
+	print request.form['name']
+	session['survey_info'] = request.form
+	return redirect('/success')
+
+
+
+@app.route('/success')
+def success():
+	render_template('success.html')	
+
+
+app.run(debug=True)	
