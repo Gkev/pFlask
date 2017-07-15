897ad1a1619c87a99343e66e4aa324ee440534de
KEVIN FORD <new_era@KEVINs-MBP.hsd1.wa.comcast.net>
2017-07-15 14:43:36 -0700

    updates

--- dojo_survey/hello.py
+++ dojo_survey/hello.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
-app.secret_key = 'what time is it?'
+app.secret_key = "what time is it?"
 
 
 @app.route('/')
@@ -13,7 +13,7 @@ def hello_world():
 @app.route('/survey', methods=['POST'])
 def survey():
 	print request.form['name']
-	session['survey_info'] = request.form
+	session['name'] = request.form
 	return redirect('/success')
 
 
